90abfeb83d6293ab096d9b3c3573fd51f7f516b5
Artem Derkachevskyi <derkachevskyiartem@gmail.com>
2024-09-23 11:14:06 +0300

    Add task 3 solutions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-//task1
+//task1 solution
 const calculator = function (num1, num2, operator) {
   if (
     typeof num1 !== "number" ||
@@ -23,6 +23,7 @@ const calculator = function (num1, num2, operator) {
 };
 
 //task2 while loop solution
+
 let whileLoopSum = 0;
 
 let i = 1;
@@ -33,7 +34,8 @@ while (i <= 100) {
 }
 console.log(whileLoopSum);
 
-//task2 do while loop solution
+//task2 do-while loop solution
+
 let doWhileLoopSum = 0;
 
 let j = 1;
@@ -46,6 +48,7 @@ do {
 console.log(doWhileLoopSum);
 
 //task2 for loop solution
+
 let forLoopSum = 0;
 
 for (i = 1; i <= 100; i++) {
@@ -54,14 +57,68 @@ for (i = 1; i <= 100; i++) {
 
 console.log(forLoopSum);
 
+/*
 //task3 while loop unlimited
 
-let answer;
+let whileAnswer;
 
-while (answer !== 6) {
-  answer = +prompt("Розв'яжіть рівняння: 2 + 2 * 2");
-  // answer = Number(prompt("Розв'яжіть рівняння: 2 + 2 * 2"));
-  answer === 6
+while (whileAnswer !== 6) {
+  whileAnswer = +prompt("Розв'яжіть рівняння: 2 + 2 * 2");
+  // whileAnswer = Number(prompt("Розв'яжіть рівняння: 2 + 2 * 2"));
+  whileAnswer === 6
     ? alert("Ви дали правильну відповідь!")
     : alert("Неправильно, спробуйте ще раз.");
 }
+    */
+
+/* //task3 do-while loop unlimited
+
+let doWhileAnswer;
+
+do {
+  doWhileAnswer = +prompt("Розв'яжіть рівняння: 2 + 2 * 2");
+  // doWhileAnswer = Number(prompt("Розв'яжіть рівняння: 2 + 2 * 2"));
+  doWhileAnswer === 6
+    ? alert("Ви дали правильну відповідь!")
+    : alert("Неправильно, спробуйте ще раз.");
+} while (doWhileAnswer !== 6); 
+ */
+
+/* 
+//task3 for loop unlimited
+
+for (i = 1; i > 0; i++) {
+  let forLoopAnswer = +prompt("Розв'яжіть рівняння: 2 + 2 * 2");
+  if (forLoopAnswer === 6) {
+    alert("Ви дали правильну відповідь!");
+    break;
+  } else {
+    alert("Неправильно, спробуйте ще раз.");
+  }
+}
+  */
+
+/*
+//task3 for loop limited
+
+let answer;
+let attempts = 3;
+
+while (attempts > 0) {
+  answer = +prompt("Розв'яжіть рівняння: 2 + 2 * 2");
+  // answer = Number(prompt("Розв'яжіть рівняння: 2 + 2 * 2"));
+
+  if (answer === 6) {
+    alert("Ви дали правильну відповідь!");
+    break;
+  } else {
+    attempts--;
+    alert(`Неправильно. У вас залишилось ${attempts} спроб(и).`);
+    // alert("Неправильно. У вас минус одна спроба.");
+  }
+
+  if (attempts === 0) {
+    alert("Ви використали всі спроби. Правильна відповідь: 6");
+  }
+}
+  */
