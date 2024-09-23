79eaecc5f2f27bb9f655acace5a8035cc696c315
Artem Derkachevskyi <derkachevskyiartem@gmail.com>
2024-09-23 10:35:02 +0300

    Add task2 solution and task3 first solution

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,8 +22,46 @@ const calculator = function (num1, num2, operator) {
   }
 };
 
-//task2
+//task2 while loop solution
+let whileLoopSum = 0;
 
+let i = 1;
 
+while (i <= 100) {
+  whileLoopSum += i;
+  i++;
+}
+console.log(whileLoopSum);
 
+//task2 do while loop solution
+let doWhileLoopSum = 0;
 
+let j = 1;
+
+do {
+  doWhileLoopSum += j;
+  j++;
+} while (j <= 100);
+
+console.log(doWhileLoopSum);
+
+//task2 for loop solution
+let forLoopSum = 0;
+
+for (i = 1; i <= 100; i++) {
+  forLoopSum += i;
+}
+
+console.log(forLoopSum);
+
+//task3 while loop unlimited
+
+let answer;
+
+while (answer !== 6) {
+  answer = +prompt("Розв'яжіть рівняння: 2 + 2 * 2");
+  // answer = Number(prompt("Розв'яжіть рівняння: 2 + 2 * 2"));
+  answer === 6
+    ? alert("Ви дали правильну відповідь!")
+    : alert("Неправильно, спробуйте ще раз.");
+}
